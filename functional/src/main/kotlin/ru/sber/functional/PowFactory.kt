96d71b7eecc6e23de5441ec99d01606375a73cf9
1Bitcoin <shigalkin@mail.ru>
2021-09-15 19:53:57 +0300

    done functional

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,10 +1,20 @@
 package ru.sber.functional
 
+import kotlin.math.*
+
 object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) {
-        TODO()
+    fun buildPowFunction(power: Int): (Any) -> Any {
+        return { value ->
+            when (value) {
+                is Int -> value.toDouble().pow(power).toInt()
+                is Double -> value.pow(power)
+                else -> println("Допустимый тип аргумента: Int, Double")
+            }
+        }
     }
 }
+
+
--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -4,7 +4,11 @@ class StudentsGroup {
 
     lateinit var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+    fun filterByPredicate(pred: (Student) -> Boolean): List<Student> {
+        if (this::students.isInitialized) {
+            return students.filter { student -> pred(student) }
+        }
+
+        return emptyList()
     }
 }
