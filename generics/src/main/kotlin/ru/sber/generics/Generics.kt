7c47b26bf12d01e71b442ddbeb16999c6ee401bd
Дмитрий l W.W <shigalkin@mail.ru>
2021-09-16 20:45:58 +0300

    done generics

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,46 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
-
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <K, V> compare(p1: Pair<K, V>, p2: Pair<K, V>): Boolean {
+    return p1 == p2
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <T> countGreaterThan(anArray: Array<T>, elem: T): Int where T : Comparable<T> {
+
+    return anArray.filter { it > elem }.size
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T> where T: Comparable<T> {
+
+    val list: MutableList<T> = mutableListOf()
+    private var insertionPoint = 0
+
+    fun add(value: T) {
+        insertionPoint = if (list.contains(value))
+            list.binarySearch(value)
+        else
+            -list.binarySearch(value) - 1
+
+        list.add(insertionPoint, value)
+    }
 }
 
 // 4.
-class Stack {
+class Stack<T> {
 
+    private val stack: MutableList<T> = mutableListOf()
+
+    fun isEmpty(): Boolean = stack.isEmpty()
+
+    fun size(): Int = stack.size
+
+    fun push(value: T) = stack.add(value)
+
+    fun pop(): T = stack.removeLast()
+
+    fun printStack() = stack.forEach { print("$it ") }
+
+    fun peek(): T = stack.last()
 }
--- generics/src/test/kotlin/ru/sber/generics/GenericsTest.kt
+++ generics/src/test/kotlin/ru/sber/generics/GenericsTest.kt
@@ -8,8 +8,8 @@ class GenericsTest {
 
     @Test
     fun comparePairsTest() {
-//        assertTrue(compare(Pair("Hello", 2L), Pair("Hello", 2L)))
-//        assertFalse(compare(Pair(BigDecimal(5), false), Pair(BigDecimal(5), true)))
+        assertTrue(compare(Pair("Hello", 2L), Pair("Hello", 2L)))
+        assertFalse(compare(Pair(BigDecimal(5), false), Pair(BigDecimal(5), true)))
     }
 
     @Test
@@ -19,24 +19,79 @@ class GenericsTest {
     }
 
     @Test
-    fun sorterTest() {
-//        val sorter = Sorter<String>()
-//        sorter.add("foo")
-//        sorter.add("hello")
-//        sorter.add("bar")
-//
-//        assertEquals(listOf("bar", "foo", "hello"), sorter.list)
+    fun sorterStringTest() {
+        val sorter = Sorter<String>()
+
+        sorter.add("foo")
+        sorter.add("hello")
+        sorter.add("bar")
+
+        assertEquals(listOf("bar", "foo", "hello"), sorter.list)
     }
 
     @Test
-    fun stackTest() {
-//        val stack = Stack<Int>()
-//        assertTrue(stack.isEmpty())
-//
-//        stack.push(5)
-//        stack.push(3)
-//        assertEquals(3, stack.pop())
-//        assertFalse(stack.isEmpty())
-//        assertEquals(5, stack.pop())
+    fun sorterIntTest() {
+        val sorter = Sorter<Int>()
+
+        sorter.add(89)
+        sorter.add(-51)
+        sorter.add(0)
+        sorter.add(5)
+        sorter.add(5)
+
+        assertEquals(listOf(-51, 0, 5, 5, 89), sorter.list)
+    }
+
+    @Test
+    fun stackIntTest() {
+        val stack = Stack<Int>()
+        assertTrue(stack.isEmpty())
+
+        stack.push(5)
+        stack.push(3)
+
+        assertEquals(3, stack.pop())
+        assertFalse(stack.isEmpty())
+        assertEquals(5, stack.pop())
+
+        stack.push(68)
+        stack.push(377)
+        stack.push(37447)
+
+        assertEquals(3, stack.size())
+
+        stack.printStack()
+
+        stack.pop()
+
+        assertEquals(377, stack.peek())
+        stack.pop()
+
+        assertEquals(68, stack.peek())
+        stack.pop()
+
+        assertTrue(stack.isEmpty())
+    }
+
+    @Test
+    fun stackStringTest() {
+        val stack = Stack<String>()
+        assertTrue(stack.isEmpty())
+
+        stack.push("abc")
+        stack.push("xxx")
+
+        assertEquals("xxx", stack.pop())
+        assertFalse(stack.isEmpty())
+        assertEquals("abc", stack.pop())
+
+        stack.printStack()
+
+        stack.push("zzz")
+        assertEquals("zzz", stack.peek())
+        assertEquals(1, stack.size())
+        stack.pop()
+
+        assertTrue(stack.isEmpty())
     }
 }
